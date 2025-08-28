875aa9d571f805d9ca44a683b82166a4bfeda4df
Sumit Kumar <106050212+sumit957602@users.noreply.github.com>
2025-08-28 18:16:16 +0530

    Add Google Analytics tracking script

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,3 +1,18 @@
 // Placeholder header script to prevent missing file errors
 // This script can be extended with actual header functionality as needed.
 console.log('header.js loaded');
+
+// Google Analytics tracking
+(function () {
+  var GA_MEASUREMENT_ID = 'G-XXXXXXXXXX'; // Replace with your Google Analytics ID
+  var gtagScript = document.createElement('script');
+  gtagScript.async = true;
+  gtagScript.src = 'https://www.googletagmanager.com/gtag/js?id=' + GA_MEASUREMENT_ID;
+  document.head.appendChild(gtagScript);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  window.gtag = gtag;
+  gtag('js', new Date());
+  gtag('config', GA_MEASUREMENT_ID);
+})();
